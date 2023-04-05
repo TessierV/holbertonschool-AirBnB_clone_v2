d1724baf616b3a68d2b20adc8cb6a17b3d0bb6be
TessierV <5628@holbertonstudents.com>
2023-04-05 02:09:44 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
-from flask import Flask
-from flask import render_template
-from models import storage
+"""
+    Create flask application instance (app)
+"""
+
+from flask import Flask, render_template
+from markupsafe import escape
 from models.state import State
+from models import storage
 
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ display a HTML page with list of states """
-    states = storage.all(State)
-    states_list = sorted(states.values(), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=states_list)
-
-
 @app.teardown_appcontext
-def close_session(exception):
-    """ remove the current SQLalchemy session """
+def teardown_db(exception):
+    """
+        after each request : remove current SQLAlchemy Session
+    """
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+        Display a HTML page:
+        h1 tag "States"
+        UL tag list of all State objects present in DBStorage
+            sorted by name(A->Z)
+            LI tag : description of one State:<state.id>: <B><state.name></B>
+    """
+    states = storage.all(State)
+
+    if states:
+        states_list = sorted(states.values(), key=lambda x: x.name)
+    else:
+        states_list = []
+    return render_template('7-states_list.html', states=states_list)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
