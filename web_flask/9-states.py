7a24c94d445753ea9a9f4b1c25d0ea1d9afc44c5
TessierV <5628@holbertonstudents.com>
2023-04-05 07:46:37 -0700

    test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,39 +1,55 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
+""" A script that starts a Flask web application with storage """
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
 def states():
-    states = storage.all(State)
-    states_list = sorted(states.values(), key=lambda x: x.name)
-    return render_template("9-states.html", states=states_list)
+    from models.state import State
+    list_state = []
+    state_item = storage.all(State).values()
+    for state in state_item:
+        list_state.append(state.to_dict())
+    list_state = sorted(list_state, key=lambda d: d['name'])
+    return render_template("7-states_list.html", state_item=list_state)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """ if found, display the list of city linked
-        to the state sorted by name, else H1 not found
-    """
-    state_id = None
-    states = storage.all(State)
-    for state in states.values():
-        if state.id == id:
-            state_id = state
-    return render_template("9-states.html", state_id=state_id)
+    from models.state import State
+    from models.city import City
+
+    list_city = []
+    state_obj = None
+    is_found = False
+
+    state_item = storage.all(State).values()
+    for state in state_item:
+        obj = state.to_dict()
+        if obj.get("id") == id:
+            state_obj = obj
+            is_found = True
+
+    city_item = storage.all(City).values()
+    for city in city_item:
+        if city.to_dict().get("state_id") == id:
+            list_city.append(city.to_dict())
+
+    list_city = sorted(list_city, key=lambda d: d['name'])
+    return render_template("9-states.html", state_item=state_obj,
+                           city_item=list_city, is_found=is_found)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """ remove the current SQLalchemy session """
-    storage.close()
+def close_db(error):
+    from models.engine.db_storage import DBStorage
+    if isinstance(storage, DBStorage):
+        storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
