dc54a3e50b5d37d4858eeb031e904fd3f6b7930c
TessierV <5628@holbertonstudents.com>
2023-04-05 03:12:10 -0700

    test

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
-from flask import Flask
-from flask import render_template
+"""
+flask model
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
-
-
-@app.route("/states", strict_slashes=False)
-def states():
-    states = storage.all(State)
-    states_list = sorted(states.values(), key=lambda x: x.name)
-    return render_template("9-states.html", states=states_list)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """ if found, display the list of city linked
-        to the state sorted by name, else H1 not found
-    """
-    state_id = None
-    states = storage.all(State)
-    for state in states.values():
-        if state.id == id:
-            state_id = state
-    return render_template("9-states.html", state_id=state_id)
+storage.all()
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """ remove the current SQLalchemy session """
+def teardown_data(self):
+    """
+        refrech data
+    """
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/states', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id=None):
+    """
+        list state by id if found
+    """
+    info = []
+    states = storage.all(State)
+    if id is None:
+        states_list = sorted(states.values(), key=lambda x: x.name)
+        for k in states:
+            info.append(states[k])
+    else:
+        id = 'State.' + id
+        info = states.get(id)
+    return render_template('9-states.html', states=info, id=id)
+    return render_template('9-states.html', state=id)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
